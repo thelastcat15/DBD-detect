32fb57c15558f5880a7b1ed24c31a665c000eed1
Fenix_15 <84333919+thelastcat15@users.noreply.github.com>
2023-10-29 07:57:45 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
+url = "https://raw.githubusercontent.com/thelastcat15/DBD-detect/main"
 
 images = os.listdir("src")
 
@@ -32,7 +33,7 @@ def is_image_in_image(template_path, uploaded_image):
 
     try :
         result = cv2.matchTemplate(img2, img1, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.5
+        threshold = 0.4
         locations = np.where(result >= threshold)
 
         return len(locations[0]) > 0
@@ -47,7 +48,7 @@ async def on_message(message):
     return
   if message.channel.id == 1167875055447978066 and len(message.attachments) == 1:
     embed = Embed(title = "Loading...", description = "", colour = 0xe67e22)
-    embed.set_image(url="attachment://./test/image.png")
+    embed.set_image(url=f"{url}/load.gif")
     embed.timestamp = datetime.utcnow()
     old_embed = await message.channel.send(embed=embed)
     
@@ -64,8 +65,8 @@ async def on_message(message):
             description = "Map Name : "+Name.replace(".png", ""),
             colour = 0x2ecc71
         )
-        embed2.set_image(url="attachment://preview/"+Name)
-        embed2.set_thumbnail(url="attachment://src/"+Name)
+        embed2.set_image(url=f"{url}/preview/{Name}")
+        embed2.set_thumbnail(url=f"{url}/src/{Name}")
         embed2.timestamp = datetime.utcnow()
     else :
         embed2 = Embed(
